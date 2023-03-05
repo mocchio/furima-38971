8804d6b4bfa14aade747f24743b4a0bd1f1b8f89
mocchio <skmt.rome@gmail.com>
2023-03-05 16:53:49 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # パスワードが半角英数字混合での入力が必須であることのバリデーション
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname, presence: true
+  validates :name_last, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :name_first, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :name_last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :name_first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :date_of_birth, presence: true
 end
