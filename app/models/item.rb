02722204e39169a27c298b2bde92116d3585a2bb
mocchio <skmt.rome@gmail.com>
2023-03-07 18:26:39 +0900

    レビュー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,6 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
   validates :image, presence: true
 end
