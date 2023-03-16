16c5d5bebd2c9f99ca06798b7f00a720c6ab74de
mocchio <skmt.rome@gmail.com>
2023-03-16 12:42:21 +0900

    サーバーコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-server '13.112.133.95', user: 'ec2-user', roles: %w{app db web}
+# server '13.112.133.95', user: 'ec2-user', roles: %w{app db web}
